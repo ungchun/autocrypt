76de3a2abaf02a770b48d0a6037fbc4d60a081cf
ungchun <leedool3003@naver.com>
2022-10-20 19:09:20 +0900

    build : set cocoapods, MVVM groups

---
+++ Pods/Target Support Files/Pods-autocrypt/Pods-autocrypt-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_autocrypt : NSObject
+@end
+@implementation PodsDummy_Pods_autocrypt
+@end
---
+++ autocrypt/View/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  autocrypt
+//
+//  Created by 김성훈 on 2022/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+}
+
