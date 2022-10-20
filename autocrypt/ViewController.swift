d044c07e892a55d93e712f471f454f7b94c34dd3
ungchun <leedool3003@naver.com>
2022-10-20 18:59:53 +0900

    Initial Commit

---
+++ autocrypt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autocrypt
+//
+//  Created by 김성훈 on 2022/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
