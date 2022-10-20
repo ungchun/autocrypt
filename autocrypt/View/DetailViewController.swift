7b390bbe0193f1877a7842b48b53551fdef2be0d
ungchun <leedool3003@naver.com>
2022-10-20 22:43:37 +0900

    test : MapView Test

--- autocrypt/View/DetailViewController.swift
+++ autocrypt/View/DetailViewController.swift
@@ -1,11 +1,260 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    let centerNameBackView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    let centerNameIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "hospital.png")
+        return imageView
+    }()
+    let centerNameTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "센터명"
+        return label
+    }()
+    var centerNameContent: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        return label
+    }()
+    lazy var centerNameStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [centerNameIcon, centerNameTitle, centerNameContent])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.backgroundColor = .green
+        stackView.spacing = 10
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    let facilityNameBackView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    let facilityNameIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "building.png")
+        return imageView
+    }()
+    let facilityNameTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "건물명"
+        return label
+    }()
+    var facilityNameContent: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    lazy var facilityNameStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [facilityNameIcon, facilityNameTitle, facilityNameContent])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.backgroundColor = .green
+        stackView.spacing = 10
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    let phoneNumberBackView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    let phoneNumberIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "telephone.png")
+        return imageView
+    }()
+    let phoneNumberTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "전화번호"
+        return label
+    }()
+    var phoneNumberContent: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        return label
+    }()
+    lazy var phoneNumberStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [phoneNumberIcon, phoneNumberTitle, phoneNumberContent])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.backgroundColor = .green
+        stackView.spacing = 10
+        stackView.alignment = .center
+        return stackView
+    }()
+
+    let updateAtBackView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    let updateAtIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "chat.png")
+        return imageView
+    }()
+    let updateAtTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "업데이트 시간"
+        return label
+    }()
+    var updateAtContent: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 2
+        return label
+    }()
+    lazy var updateAtStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [updateAtIcon, updateAtTitle, updateAtContent])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.backgroundColor = .green
+        stackView.spacing = 10
+        stackView.alignment = .center
+        return stackView
+    }()
+    
+    let addressBackView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    let addressIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "placeholder.png")
+        return imageView
+    }()
+    let addressTitle: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "주소"
+        return label
+    }()
+    var addressContent: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    lazy var addressStackView: UIStackView = {
+       let stackView = UIStackView(arrangedSubviews: [addressIcon, addressTitle, addressContent])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.backgroundColor = .green
+        stackView.spacing = 10
+        stackView.alignment = .center
+        return stackView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .gray
+        
+        centerNameContent.text = "centerNameContent"
+        view.addSubview(centerNameBackView)
+        centerNameBackView.addSubview(centerNameStackView)
+        centerNameIcon.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+        }
+        centerNameStackView.snp.makeConstraints { make in
+            make.width.height.equalTo(150)
+            make.center.equalToSuperview()
+        }
+        centerNameBackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(100)
+            make.left.equalToSuperview().offset(10)
+            make.width.height.equalTo(180)
+        }
+        
+        facilityNameContent.text = "facilityNameContent"
+        view.addSubview(facilityNameBackView)
+        facilityNameBackView.addSubview(facilityNameStackView)
+        facilityNameIcon.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+        }
+        facilityNameStackView.snp.makeConstraints { make in
+            make.width.height.equalTo(150)
+            make.center.equalToSuperview()
+        }
+        facilityNameBackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(100)
+            make.right.equalToSuperview().offset(-10)
+            make.width.height.equalTo(180)
+        }
+        
+        phoneNumberContent.text = "phoneNumberContent"
+        view.addSubview(phoneNumberBackView)
+        phoneNumberBackView.addSubview(phoneNumberStackView)
+        phoneNumberIcon.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+        }
+        phoneNumberStackView.snp.makeConstraints { make in
+            make.width.height.equalTo(150)
+            make.center.equalToSuperview()
+        }
+        phoneNumberBackView.snp.makeConstraints { make in
+            make.top.equalTo(centerNameBackView.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(10)
+            make.width.height.equalTo(180)
+        }
+        
+        updateAtContent.text = "updateAtContent"
+        view.addSubview(updateAtBackView)
+        updateAtBackView.addSubview(updateAtStackView)
+        updateAtIcon.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+        }
+        updateAtStackView.snp.makeConstraints { make in
+            make.width.height.equalTo(150)
+            make.center.equalToSuperview()
+        }
+        updateAtBackView.snp.makeConstraints { make in
+            make.top.equalTo(facilityNameBackView.snp.bottom).offset(20)
+            make.right.equalToSuperview().offset(-10)
+            make.width.height.equalTo(180)
+        }
+        
+        addressContent.text = "addressContent"
+        view.addSubview(addressBackView)
+        addressBackView.addSubview(addressStackView)
+        addressIcon.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+        }
+        addressStackView.snp.makeConstraints { make in
+            make.width.height.equalTo(150)
+            make.center.equalToSuperview()
+        }
+        addressBackView.snp.makeConstraints { make in
+            make.top.equalTo(phoneNumberBackView.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(10)
+            make.right.equalToSuperview().offset(-10)
+            make.height.equalTo(180)
+        }
         
         self.title = "센터명"
         self.navigationController?.navigationBar.topItem?.backButtonTitle = ""
--- autocrypt/View/MapViewController.swift
+++ autocrypt/View/MapViewController.swift
@@ -1,12 +1,99 @@
 import UIKit
+import MapKit
+import SnapKit
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    let sesacLocationButton: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .red
+        button.titleLabel?.text = "접종센터로"
+        return button
+    }()
+    let myLocationButton: UIButton = {
+       let button = UIButton()
+        button.backgroundColor = .blue
+        button.titleLabel?.text = "현재위치로"
+        return button
+    }()
+    let mapView = MKMapView()
+    
+    var locationManager: CLLocationManager?
+    var currentLocation: CLLocationCoordinate2D?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
         
         self.title = "지도"
         self.navigationController?.navigationBar.topItem?.backButtonTitle = "Back"
+        
+        self.locationManager = CLLocationManager()
+        self.locationManager?.delegate = self
+        self.locationManager?.requestWhenInUseAuthorization()
+        
+        mapView.delegate = self
+        
+        view.addSubview(mapView)
+        
+        mapView.addSubview(myLocationButton)
+        mapView.addSubview(sesacLocationButton)
+        
+        
+
+        mapView.snp.makeConstraints { make in
+            make.top.bottom.left.right.equalToSuperview()
+        }
+        
+        myLocationButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-140)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalTo(50)
+        }
+        
+        sesacLocationButton.snp.makeConstraints { make in
+            make.top.equalTo(myLocationButton.snp.bottom).offset(10)
+            make.left.equalToSuperview().offset(20)
+            make.right.equalToSuperview().offset(-20)
+            make.height.equalTo(50)
+        }
+        
+        
+    
+        // 처음에는 대구광역시로 세팅
+        let coordinate = CLLocationCoordinate2D(latitude: 35.8714354, longitude: 128.601445)
+        let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        mapView.addAnnotation(pin)
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        //location5
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("GPS 권한 설정됨")
+            self.locationManager?.startUpdatingLocation() // 중요!
+            
+            guard let locationManager else { return }
+            currentLocation = locationManager.location?.coordinate
+            guard let currentLocation else { return }
+            let coordinate = CLLocationCoordinate2D(latitude: currentLocation.latitude, longitude: currentLocation.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+            
+            let pin = MKPointAnnotation()
+            pin.coordinate = coordinate
+            mapView.addAnnotation(pin)
+        case .restricted, .notDetermined:
+            print("GPS 권한 설정되지 않음")
+        case .denied:
+            print("GPS 권한 요청 거부됨")
+        default:
+            print("GPS: Default")
+        }
     }
 }
